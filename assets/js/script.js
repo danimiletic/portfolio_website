2818e614308c7b944015644c217ef72a165f3046
dani miletic <danimiletic@gmail.com>
2023-05-09 16:44:19 -0600

    created chessboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY > 20){
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if(this.scrollY > 20) {
       $('.navbar').addClass("sticky");
-    }else{
+    } else {
       $('.navbar').removeClass("sticky");
     }
   })
   // toggle menu/navbar
-  $('.menu-btn').click(function(){
+  $('.menu-btn').click(function() {
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
   });
